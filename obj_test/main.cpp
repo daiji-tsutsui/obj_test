2f69eee34a1a7863e1d795ee6bc9686f68909c21
Daiji Tsutsui <41978972+daiji-tsutsui@users.noreply.github.com>
2018-10-24 17:16:29 +0900

    Initial Commit

---
+++ obj_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  obj_test
+//
+//  Created by 筒井 大二 on 2018/10/24.
+//  Copyright © 2018年 筒井 大二. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
